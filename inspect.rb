5ee662c6b713813802e7b8731734aa26478c866b
ttakahashi <ttakahashi@up-frontier.jp>
2012-11-13 18:59:17 +0900

    date, channel, sitecode reading is done

--- inspect.rb
+++ inspect.rb
@@ -1,3 +1,10 @@
-
-
-p line.match(%r(^([^ ]*) ([^ ]*) ([^ ]*)))
+require 'pp'
+fp = File.open("dummy.log", "r")
+fp.each{ |line|
+	trimmed = line.match(%r(^([^ ]*) ([^ ]*) ([^ ]*) ([^c]*) ([^ ]*) ([^ ]*)  ([^ ]*) ([^ ]*)))
+		date =  trimmed[2] + ' GMT +0000]'
+		channel = trimmed[6]
+		sitecode = trimmed[8]
+		pair = trimmed[6] + '_' + trimmed[8]
+		
+}
