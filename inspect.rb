354cd74c335f6c6272257e4c5f39ceb833d3927c
ttakahashi <ttakahashi@up-frontier.jp>
2012-11-14 12:13:46 +0900

    finish, hasten to commit

--- inspect.rb
+++ inspect.rb
@@ -21,9 +21,8 @@ fp = File.open(ARGV[0], "r")
 fp.each{ |line|
   date, pair = trim(line)
 
-
   read_time =  Time.parse(date)
-  if read_time >= strict_time + 60 * 5
+  while read_time >= strict_time + 60 * 5
     strict_time = strict_time + 60 * 5
     list.store(strict_time, Hash.new())
   end
@@ -35,7 +34,7 @@ fp = File.open(ARGV[0], "r")
 h = Hash.new()
 fp.each{ |line|
   date, pair = trim(line) # pair is channel and sitecode
-pair.chomp!
+  pair.chomp!
   if h[:pair] == nil
     h.store(pair, 0)
   end
@@ -45,16 +44,47 @@ fp.close
 # connect pair to list
 comp_list = Hash.new()
 list.each{ |key, value|
-  value = h
-comp_list.store(key, value)
+  value = h.dup
+  comp_list.store(key, value)
 }
 
 # count start with comp_list
+strict_time = Time.mktime(2012, 9, 28, 19, 00, 00)
 fp = File.open(ARGV[0], "r")
 fp.each{ |line|
   date, pair = trim(line)
-pair.chomp!
-  comp_list[:date][:pair]  += 1
+  pair.chomp!
+  read_time =  Time.parse(date)
+
+  while read_time >= strict_time + 60 * 5
+    strict_time = strict_time + 60 * 5
+  end
+  if read_time <= strict_time + 60 * 5
+    comp_list[strict_time][pair]  += 1
+  end
+}
+fp.close
+
+fp = File.open("result", "w")
+comp_list.each{ |key2, value2|
+  value2.each{ |key3, value3|
+    fp.write(key3)
+    fp.write(",")
+  }
+  fp.write("\n")
+  break
 }
 
-pp comp_list
+fp.close
+fp = File.open("result", "a")
+comp_list.each{ |key1, value1|
+  fp.write(key1)
+  fp.write(", ")
+  value1.each{ |key2, value2|
+    fp.write(value2)
+    fp.write(",")
+  }
+  fp.write("\n")
+}
+
+
